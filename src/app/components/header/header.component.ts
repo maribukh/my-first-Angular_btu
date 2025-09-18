141ae1c2ae290365e5ed3fc9eebca54b28289f88
Mariam <mariebukhaidze@gmail.com>
2025-09-18 12:11:26 +0400

    Draft mobile dropdown

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -9,4 +9,17 @@ import { DropdownComponent } from '../ui/dropdown/dropdown.component';
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss'],
 })
-export class HeaderComponent {}
+export class HeaderComponent {
+  isMenuOpen = false;
+
+  toggleMenu(): void {
+    this.isMenuOpen = !this.isMenuOpen;
+
+    // Prevent body scroll when menu is open
+    if (this.isMenuOpen) {
+      document.body.classList.add('no-scroll');
+    } else {
+      document.body.classList.remove('no-scroll');
+    }
+  }
+}
